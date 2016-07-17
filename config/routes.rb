9094d7c40862f0d8554d631dd71507e65fb62a13
JElbourne <jayelbourne@gmail.com>
2016-07-17 15:53:43 -0400

    Removed the use of the Heroku Host for subscriptions controller. This was originally a test for SSL situations, but did not work so reverted back to standard Routes with the SSl Cert on the Aliasnerd.com domain.

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :projects, except: :show
   resources :videos
-  if Rails.env.production?
-    constraints(:host => /herokuapp.com/) do
-      resource :subscription
-    end
-  else
-    resource :subscription
-  end
+  resource :subscription
 
   resource :card
   resource :config_nerd, only: [:edit, :update], path: "admin"
  
-  get '/subscription/new', to: redirect('https://aliasnerd.herokuapp.com/subscription/new')
-  get '/subscription/edit', to: redirect('https://aliasnerd.herokuapp.com/subscription/edit')
   get "/search", to: "search#index", as: "search" 
   post "/liked", to: "votes#vote_up", as: "vote_up"
   root to: 'welcome#index'
